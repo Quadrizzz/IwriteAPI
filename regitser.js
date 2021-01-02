56220de9fd59c1bcc3b09d3f77e0e7e1f3b81679
Quadrizz <mackquadrizz@gmail.com>
2021-01-02 23:41:39 +0100

    fixed transaction issues

--- regitser.js
+++ regitser.js
@@ -2,30 +2,38 @@ const register = (req, res, database, bcrypt)=>{
     const {name, email, penname,} = req.body;
     const hash = bcrypt.hashSync(req.body.password)
     if(!name || !email || !penname){
-        return res.status(400).json("Enter Proper Credentials")
+        res.status(400).json("Enter Proper Credentials")
     }
-    
-    database('users').insert({
-        name: req.body.name,
-        email: req.body.email,
-        penname: req.body.penname,
-        password: hash
+
+    database.transaction(trx => {
+        trx.insert({
+            penname : penname,
+            password: hash
+        })
+        .into('login')
+        .returning('penname')
+        .then((loginPenname) => {
+            return  trx('users')
+            .returning('*')
+            .insert({
+                name: req.body.name,
+                email: req.body.email,
+                penname: loginPenname[0],
+                password: hash
+            })
+            .then( (user) =>{ 
+                res.json(user[0])
+            })
+        
+
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
+
     })
-    .catch(err => {
-        return res.status(400).json(err)
-    })
-    .then( () =>{ return database('login').insert({
-        penname: req.body.penname,
-        password: hash
-    })}
-    )
-    .then( database.select().from('users').where('email',`${req.body.email}`)
-            .then((users)=> {
-                return res.json(users)
-            }))
     .catch(err => {
         console.log(err)
-        return res.status(400).json(err)
+        res.status(400).json(err)
     })
 
 
